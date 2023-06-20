c82102408e030fb159d89867c10753749c0c88e9
agontar <agontar@yandex.ru>
2023-06-20 11:18:56 +0300

    add simple insertion for comparison

--- Algo/DZ4/src/App.java
+++ Algo/DZ4/src/App.java
@@ -1,31 +1,38 @@
 public class App {
     private static Node root = null;
+
     public static void main(String[] args) throws Exception {
-        
-        LLRBTREE tree = new LLRBTREE();
-     
-    root = tree.insert(root, 10);
-      // корневой элемент черный
-    
-    root.color = false;
-     
-    root = tree.insert(root, 20);
-    root.color = false;
-     
-    root = tree.insert(root, 30);
-    root.color = false;
-     
-    root = tree.insert(root, 40);
-    root.color = false;
-     
-    root = tree.insert(root, 50);
-    root.color = false;
-     
-    root = tree.insert(root, 25);
-    root.color = false;
- 
-    // центрированный обход и распечатка    
-    tree.inorder(root);
-    tree.DispTree(root); 
+       int [] data = {20, 10, 40, 30, 25, 50};
+        System.out.println("Без балансировки:");
+        LLRBTREE simpleTree = new LLRBTREE();
+        root = simpleTree.simpleInsert(root, data[0]);
+        root = simpleTree.simpleInsert(root, data[1]);
+        root = simpleTree.simpleInsert(root, data[2]);
+        root = simpleTree.simpleInsert(root, data[3]);
+        root = simpleTree.simpleInsert(root, data[4]);
+        root = simpleTree.simpleInsert(root, data[5]);
+        // центрированный обход и распечатка
+        simpleTree.inorder(root);
+        simpleTree.DispTree(root);
+
+        System.out.println("То же самое с балансировкой:");
+        root = null;
+        LLRBTREE balancedTree = new LLRBTREE();
+        root = balancedTree.insert(root, data[0]);
+        // корневой элемент черный
+        root.color = false;
+        root = balancedTree.insert(root, data[1]);
+        root.color = false;
+        root = balancedTree.insert(root, data[2]);
+        root.color = false;
+        root = balancedTree.insert(root, data[3]);
+        root.color = false;
+        root = balancedTree.insert(root, data[4]);
+        root.color = false;
+        root = balancedTree.insert(root, data[5]);
+        root.color = false;
+        // центрированный обход и распечатка
+        balancedTree.inorder(root);
+        balancedTree.DispTree(root);
     }
 }
--- Algo/DZ4/src/LLRBTREE.java
+++ Algo/DZ4/src/LLRBTREE.java
@@ -1,146 +1,148 @@
 
-
 public class LLRBTREE {
-          
+
     // вспомогательный метод поворота против часовой стрелки.
-    Node rotateLeft(Node myNode)
-    {
-        System.out.printf("left rotation!!\n");
+    Node rotateLeft(Node myNode) {
+        System.out.printf("left rotation\n");
         Node child = myNode.right;
         Node childLeft = child.left;
- 
+
         child.left = myNode;
-        myNode.right = childLeft;        
- 
+        myNode.right = childLeft;
+
         return child;
     }
- 
+
     // вспомогательный метод поворота по часовой стрелке.
-    Node rotateRight(Node myNode)
-    {
+    Node rotateRight(Node myNode) {
         System.out.printf("right rotation\n");
         Node child = myNode.left;
         Node childRight = child.right;
- 
+
         child.right = myNode;
         myNode.left = childRight;
- 
+
         return child;
     }
- 
-    // проверка красный узел или нет    
-    boolean isRed(Node myNode)
-    {
+
+    // проверка красный узел или нет
+    boolean isRed(Node myNode) {
         if (myNode == null)
             return false;
         return (myNode.color == true);
     }
- 
+
     // метеод обмена цветами между узлами
-    
-    void swapColors(Node node1, Node node2)
-    {
+
+    void swapColors(Node node1, Node node2) {
         boolean temp = node1.color;
         node1.color = node2.color;
         node2.color = temp;
     }
- 
-    // вставка в левостороннее красно-черное дерево.
-    Node insert(Node myNode, int data)
-    {
-        // обычная вставка в бинарное дерево
-        
+
+    Node simpleInsert(Node myNode, int data) {
         if (myNode == null)
             return new Node(data);
- 
+
+        if (data < myNode.data)
+            myNode.left = simpleInsert(myNode.left, data);
+
+        else if (data > myNode.data)
+            myNode.right = simpleInsert(myNode.right, data);
+
+        else
+            return myNode;
+        return myNode;    
+    }
+
+    // вставка в левостороннее красно-черное дерево.
+    Node insert(Node myNode, int data) {
+        // обычная вставка в бинарное дерево
+
+        if (myNode == null)
+            return new Node(data);
+
         if (data < myNode.data)
             myNode.left = insert(myNode.left, data);
-         
+
         else if (data > myNode.data)
             myNode.right = insert(myNode.right, data);
-         
+
         else
-            return myNode; 
- 
+            return myNode;
+
         // вариант 1.
-        // правый сын красный, а левый черный или не существует        
-        if (isRed(myNode.right) && !isRed(myNode.left))
-        {
+        // правый сын красный, а левый черный или не существует
+        if (isRed(myNode.right) && !isRed(myNode.left)) {
             // левый разворот узла
-           
+
             myNode = rotateLeft(myNode);
- 
+
             // обмен цветами
             // потомок должен быть красным
             swapColors(myNode, myNode.left);
- 
+
         }
- 
+
         // вариант 2
         // левый сын и его сын красные
-        if (isRed(myNode.left) && isRed(myNode.left.left))
-        {
-            // правый разворот текущего узла            
+        if (isRed(myNode.left) && isRed(myNode.left.left)) {
+            // правый разворот текущего узла
             myNode = rotateRight(myNode);
             swapColors(myNode, myNode.right);
         }
- 
- 
+
         // вариант 3
         // и левый и правый сыны красные
-        if (isRed(myNode.left) && isRed(myNode.right))
-        {
+        if (isRed(myNode.left) && isRed(myNode.right)) {
             // изменить цвет узла
             // и его правого и левого детей
             myNode.color = !myNode.color;
- 
+
             // меняем цвета детей на черный
             myNode.left.color = false;
             myNode.right.color = false;
         }
- 
+
         return myNode;
     }
- 
- 
+
     // центрированный обход
-    void inorder(Node node)
-    {
-        if (node != null)
-        {
+    void inorder(Node node) {
+        if (node != null) {
             inorder(node.left);
             System.out.print(node.data + " ");
             inorder(node.right);
         }
     }
-    // распечатка дерева (вершина слева)
+
+    // распечатка дерева (вершина слева, движимся вправо, как будто вниз)
     int COUNT = 5;
-    void PrintTree(Node root, int space)
-        {            
-            if (root == null)
-                return;
 
-            // Увеличить расстояние между уровнями
-            space += COUNT;
+    void PrintTree(Node root, int space) {
+        if (root == null)
+            return;
 
-            // Правый сын сверху
-            PrintTree(root.right, space);
+        // Увеличить расстояние между уровнями
+        space += COUNT;
 
-            // Текущий узел
-            // 
+        // Правый сын сверху
+        PrintTree(root.right, space);
 
-            System.out.println();
-            for (int i = COUNT; i < space; i++)
-                System.out.print(" ");
-            System.out.print(root.data + "\n");
+        // Текущий узел
+        //
 
-            // Левый сын снизу
-            PrintTree(root.left, space);
-        }
+        System.out.println();
+        for (int i = COUNT; i < space; i++)
+            System.out.print(" ");
+        System.out.print(root.data + "\n");
 
-        void DispTree(Node root)
-        {
-            // Изначальный промежуток 0
-            PrintTree(root, 0);
-        }
+        // Левый сын снизу
+        PrintTree(root.left, space);
+    }
+
+    void DispTree(Node root) {
+        // Изначальный промежуток 0
+        PrintTree(root, 0);
+    }
 }
