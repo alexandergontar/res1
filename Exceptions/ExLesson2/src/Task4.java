0c294c40d2042e833bb7379dadde1feb4769b149
agontar <agontar@yandex.ru>
2023-06-27 10:10:49 +0300

    workout Task4

--- Exceptions/ExLesson2/src/Task4.java
+++ Exceptions/ExLesson2/src/Task4.java
@@ -1,28 +1,25 @@
 import java.util.Scanner;
-public class Task4 {
-    static float inputFloatNumber() {
-        Scanner scan = new Scanner(System.in, "Cp866");
-        float result = 0;
-        while (true) {
-            try {
-                System.out.print("Ведите  число: ");
-                String inputNumber = scan.nextLine();
-                if(inputNumber.trim().isEmpty()){
-                    System.out.println("String is empty or contains spaces only!");
-                }               
-                
-                result = Float.parseFloat(inputNumber);                
-                scan.close();
-                break;
-            } catch (NumberFormatException e) {                
-                System.out.println("Check your input!!!");
-            }
 
+public class Task4 {
+    static String inputString() {
+        Scanner scan = new Scanner(System.in, "Cp866");
+        System.out.print("Ведите сообщение: ");
+        String input = scan.nextLine();
+        if (input.trim().isEmpty()) {            
+            scan.close();
+            throw new RuntimeException("Пусоте сообщение!\nСообщение должно иметь содержание.");
         }
-        return result;
+        scan.close();
+        return input;
     }
+
     public static void main(String[] args) throws Exception {
         System.out.println("Task4");
-        System.out.println(inputFloatNumber());
+        try {
+           System.out.println(inputString()); 
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+        
     }
 }
