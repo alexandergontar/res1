38996f599e58c7d64cde950cf144f4460af6be14
agontar <agontar@yandex.ru>
2023-07-01 08:54:07 +0300

    add comments

--- Exceptions/ExLesson3/src/App.java
+++ Exceptions/ExLesson3/src/App.java
@@ -11,14 +11,14 @@ import java.util.Scanner;
 
 public class App {
 
-    static String[] inputStringData() throws RuntimeException{
+    static String[] inputStringData() throws InputFormatException {
         Scanner scan = new Scanner(System.in, "Cp866");
         System.out.println("Ведите Ваши данные через пробел. ");
         System.out.println("Формат: Фамилия Имя Отчество №телефона дата_рождения(дд-мм-гггг) пол(f/m)  ");
         String input = scan.nextLine();
         if (input.trim().isEmpty()) { // если введна пустая строка или пробелы
             scan.close();
-            throw new RuntimeException("Пустое сообщение!\nСообщение должно иметь содержание.");
+            throw new InputFormatException("Пустое сообщение!\nСообщение должно иметь содержание.");
         }
         String[] inputData = input.split(" ");
         scan.close();
@@ -28,39 +28,39 @@ public class App {
     static void writeToTextFile(String[] data, String path) throws IOException {
         File file = new File(path);
         boolean fileExists = false;
-        if(file.exists()) {
+        if (file.exists()) {
             fileExists = true;
             System.out.println("Exists!!!");
         }
         try (FileOutputStream stream = new FileOutputStream(file, fileExists);
                 BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stream));) {
-            if(fileExists){writer.write("\n");}
+            if (fileExists) {
+                writer.write("\n");
+            }
             for (int i = 0; i < data.length; i++) {
                 writer.write(data[i] + " ");
-                
-                // writer.newLine();
+
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
             System.out.println(e.toString());
         }
 
     }
-     
 
-    static boolean validateJavaDate(String strDate) {
+    static boolean validateJavaDate(String stringDate) {
 
-        if (strDate.trim().equals("")) {
+        if (stringDate.trim().equals("")) {
             return true;
         } else {
-            SimpleDateFormat sdfrmt = new SimpleDateFormat("dd-MM-yyyy");
-            sdfrmt.setLenient(false);
+            SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+            format.setLenient(false);
             try {
-                sdfrmt.parse(strDate);
-                System.out.println(strDate + " is valid date format");
+                format.parse(stringDate);
+                System.out.println(stringDate + " is valid date format");
             }
             /* Date format is invalid */
             catch (ParseException e) {
-                System.out.println(strDate + " is Invalid Date format");
+                System.out.println(stringDate + " is Invalid Date format");
                 return false;
             }
             /* Return true if date format is valid */
@@ -68,27 +68,45 @@ public class App {
         }
     }
 
+    static boolean validateTelNumber(String number) {
+        try {
+            Integer.parseInt(number);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+
+    }
+
     static void checkInputFormat(String[] data) throws InputFormatException {
         if (data.length != 6) {
             throw new InputFormatException("Количество данных не соответствуют формату.");
         }
+        if (!validateTelNumber(data[3])) {
+            throw new InputFormatException("Негодный тел. номер!");
+        }
         if (!validateJavaDate(data[4])) {
             throw new InputFormatException("Неверная дата рождения!");
         }
-        if (!data[5].equals("m") ) {
-           if (!data[5].equals("f")) {
-            throw new InputFormatException("Неправильный указатель пола ");
-           } 
+        if (!data[5].equals("m")) {
+            if (!data[5].equals("f")) {
+                throw new InputFormatException("Неправильный указатель пола ");
+            }
         }
 
     }
 
     public static void main(String[] args) throws Exception {
 
-        String[] inputData = inputStringData();
-        System.out.println(Arrays.toString(inputData));
-        checkInputFormat(inputData);        
-        writeToTextFile(inputData, inputData[0]  + ".txt");
+        try {
+            String[] inputData = inputStringData();
+            System.out.println(Arrays.toString(inputData));
+            checkInputFormat(inputData);
+            writeToTextFile(inputData, inputData[0] + ".txt");
+
+        } catch (InputFormatException | IOException e) {
+            System.out.println(e.toString());
+        }
 
     }
 }
